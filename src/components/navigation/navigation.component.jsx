22059620033740ca00c8b31179df9819637c3716
Dinesh <dineshkumar.software@gmail.com>
2022-10-30 16:24:42 +0530

    create navigation component

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,17 @@
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        <div className='navigation'>
+            <div className='Logo'>CheckList</div>
+            <div className="links">
+                <li>Link1</li>
+                <li>Link2</li>
+                <li>Link3</li>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navigation;
