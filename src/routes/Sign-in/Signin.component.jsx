b9cf9ff112e3e84cb4c753de12a8ce74002bfc57
Dinesh <dineshkumar.software@gmail.com>
2022-11-01 22:17:05 +0530

    Google signin and Normal Form Fields

--- src/routes/Sign-in/Signin.component.jsx
+++ src/routes/Sign-in/Signin.component.jsx
@@ -1,23 +1,44 @@
-import './Signin.styles.scss'
+import "./Signin.styles.scss"
 
-import { signInwithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils"
+import { useState } from "react";
 
-const Signin = () =>{
-    const logGoogleUser = async () =>{
-        const {user} = await signInwithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
+import { signInwithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+
+const logGoogleUser = async () => {
+    const { user } = await signInwithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+}
+
+const defaultFormFields = {
+    display: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+}
+
+const Signin = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields);
+
+    const handleChange = (event) => {
+        setFormFields({ ...formFields, [event.target.name]: event.target.value });
     }
 
     return (
-        <div className="loginForm">
-            <button onClick={logGoogleUser} className="Google">Sign In With Google</button>
-            <form onSubmit={()=>{}}>
-                <input type="text" placeholder='Display Name' required/>
-                <input type="email" placeholder='Email Address' required/>
-                <input type="password" placeholder='Password' name="pass" id="p" required/>
-                <button className="Submit">Sign Up</button>
-            </form>
-        </div>    
+        <div className="Signup">
+            <h2>Sign-in Page</h2>
+            <div className="loginForm">
+                <button onClick={logGoogleUser} className="Google">Sign In With Google</button>
+                <form onSubmit={() => { }}>
+                    <input type="text" placeholder='Display Name' name="display" onChange={handleChange} value={formFields.display} required />
+                    <input type="email" placeholder='Email Address' name="email" onChange={handleChange} value={formFields.email} required />
+                    <input type="password" placeholder='Password' name="password" onChange={handleChange} value={formFields.password} required />
+                    <input type="password" placeholder='Confirm Password' name="confirmPassword" onChange={handleChange} value={formFields.confirmPassword} required />
+                    <button className="Submit">Sign Up</button>
+                </form>
+            </div>
+        </div>
+
     )
 }
 
