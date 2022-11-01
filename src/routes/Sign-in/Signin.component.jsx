0f49ecbf65102e6630055c9d13c6cca3cb712ab1
Dinesh <dineshkumar.software@gmail.com>
2022-11-01 19:40:28 +0530

    Basic Sign-in

--- src/routes/Sign-in/Signin.component.jsx
+++ src/routes/Sign-in/Signin.component.jsx
@@ -1,7 +1,24 @@
+import './Signin.styles.scss'
 
+import { signInwithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils"
 
 const Signin = () =>{
-    return <h1>Sign-in Page</h1>
+    const logGoogleUser = async () =>{
+        const {user} = await signInwithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    }
+
+    return (
+        <div className="loginForm">
+            <button onClick={logGoogleUser} className="Google">Sign In With Google</button>
+            <form onSubmit={()=>{}}>
+                <input type="text" placeholder='Display Name' required/>
+                <input type="email" placeholder='Email Address' required/>
+                <input type="password" placeholder='Password' name="pass" id="p" required/>
+                <button className="Submit">Sign Up</button>
+            </form>
+        </div>    
+    )
 }
 
 export default Signin;
