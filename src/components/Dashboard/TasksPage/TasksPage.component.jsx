98f61ee4f25760192d4a3cdbd34c21c8d9bd3723
Dinesh <dineshkumar.software@gmail.com>
2022-12-01 23:37:21 +0530

    Fix overdue

--- src/components/Dashboard/TasksPage/TasksPage.component.jsx
+++ src/components/Dashboard/TasksPage/TasksPage.component.jsx
@@ -4,21 +4,21 @@ import { Fragment, useState, useEffect } from "react"
 import { FetchTaskInDB } from "../../../utils/firebase/firebase.utils"
 import TaskPopup from "./task/Popups/CreateTaskPopup/TaskPopup.component"
 import Task from "./task/task.component"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 const TaskPage = () => {
     const [createTask, setCreateTask] = useState(false)
     const [updateTask, setUpdateTask] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
 
-    const [pendingTask, setPendingTask] = useState(false)
+    const [pendingTask, setPendingTask] = useState(true)
     const [overdueTask, setOverdueTask] = useState(false)
     const [completedTask, setCompletedTask] = useState(false)
 
     const [tasks, setTasks] = useState([])
 
     useEffect(() => {
-        FetchTaskInDB().then((resp) => setTasks(resp)).then(setIsLoading(false));
+        FetchTaskInDB().then((resp) => setTasks(resp)).finally(setIsLoading(false));
     }, [])
 
     useEffect(() => {
@@ -37,15 +37,16 @@ const TaskPage = () => {
             <div className="my-tasks">
                 <h2>My Tasks</h2>
 
-                {tasks.length === 0 ? <div className="empty">No tasks were created!</div> : <AnimatePresence>
+                {tasks.length === 0 ? <div className="empty">No tasks were created!</div> : <Fragment>
                     <div className="tasks-wrapper">
                         <div className="header"><motion.span animate={{ rotate: overdueTask ? 0 : -90 }} className="material-symbols-outlined" onClick={() => setOverdueTask(!overdueTask)}>expand_more</motion.span>Overdue Tasks</div>
                         {overdueTask && <motion.div initial={{ y: -50 }} animate={{ y: 0 }} exit={{ y: -50 }} className="task-list">
                             {tasks.map(obj => {
-                                const dueDate = new Date(obj.dueDate.seconds)
+                                const dueDate = new Date(0)
+                                dueDate.setUTCSeconds(+obj.dueDate.seconds)
                                 const currentDate = new Date()
-
-                                if (dueDate > currentDate && !obj.taskDone) {
+                                console.log(dueDate)
+                                if (currentDate > dueDate && !obj.taskDone) {
                                     return <Task key={obj.id} {...obj} reRenderFunction={ReRender} />
                                 }
                                 return null;
@@ -76,7 +77,7 @@ const TaskPage = () => {
                             })}
                         </motion.div>}
                     </div>
-                </AnimatePresence>}
+                </Fragment>}
             </div>
             <motion.button drag dragConstraints={{ top: 0, bottom: 0, right: 0, left: 0 }} className='Bottom custom-button' onClick={() => { setCreateTask(true) }}><span className="material-symbols-outlined image">add</span><span className="xdxd">Create Task</span></motion.button>
         </Fragment >
--- src/components/Sidebar/DropdownButton/DButton.component.jsx
+++ src/components/Sidebar/DropdownButton/DButton.component.jsx
@@ -1,19 +1,10 @@
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-
-import { motion } from "framer-motion"
 
 const Dbutton = ({ Name, L, State }) => {
-    const [open, setOpen] = useState(false);
-
-    const HandleClick = () => {
-        setOpen(!open);
-    }
-
     return (
         <Fragment>
-            <Link to={L} className={State ? "active" : null}><motion.span animate={{ rotate: open ? 0 : -90 }} className="material-symbols-outlined" onClick={HandleClick}>expand_more</motion.span>{Name}</Link>
+            <Link to={L} className={State ? "active" : null}>{Name}</Link>
         </Fragment >
     )
 }
