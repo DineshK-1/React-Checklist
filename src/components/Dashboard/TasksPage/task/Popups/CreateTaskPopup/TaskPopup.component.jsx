1af248f7cbe4cd24b249ddbbc58afc28772fea0e
Dinesh <dineshkumar.software@gmail.com>
2022-12-15 17:27:06 +0530

    Tags

--- src/components/Dashboard/TasksPage/task/Popups/CreateTaskPopup/TaskPopup.component.jsx
+++ src/components/Dashboard/TasksPage/task/Popups/CreateTaskPopup/TaskPopup.component.jsx
@@ -11,38 +11,60 @@ const TaskPopup = ({ open, onClose, reRenderFunction }) => {
 
     const nameRef = useRef();
     const descriptionRef = useRef();
+    const tagRef = useRef();
 
     const { AddAlert } = useContext(AlertsContext);
 
     const [date, setDate] = useState(new Date());
+    const [tags, setTags] = useState([]);
     if (!open) return null
 
     const HandleSubmit = () => {
         CreateTaskInDB(nameRef, descriptionRef, date);
         onClose();
+        clearTags();
         AddAlert("success", "Task Added Successfully");
         reRenderFunction();
+    }
+    const clearTags = () => {
+        setTags([]);
+    }
+
+    const AddTags = () => {
+        setTags((tag) => {
+            return tag.concat({ ID: tag.length, name: tagRef.current.value })
+        })
     }
     return (
         <Fragment>
             <div className="popup-overlay"></div>
             <div className="Popup">
-                <button onClick={onClose} className="CloseButton"><span className="material-symbols-outlined rotateanim">close</span></button>
+                <button onClick={()=>{clearTags();onClose()}} className="CloseButton"><span className="material-symbols-outlined rotateanim">close</span></button>
                 <div className="header-wrap">
                     <div className="title-wrap">
                         <div className="title">Task Name</div>
                         <input className="text-input" ref={nameRef} type="text" placeholder="Task Name..." required />
                     </div>
-                    <div className="tags">
-                        <div className="dropdown">
-                            <button className="custom-button">Tags</button>
-                            <div className="dropdown-options">
-                                <span>School</span>
-                                <span>work</span>
-                                <span>Create Label</span>
-                            </div>
+                </div>
+
+                <div className="tags-wrapper">
+
+                    <div className="title">Tags:</div>
+                    <input className="text-input" ref={tagRef} type="text" placeholder="Press enter to add tags" onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            AddTags();
+                        }
+                    }} />
+                    {tags.length === 0 ?
+                        <div className="tags">No tags added</div>
+                        :
+                        <div className="tags">
+                            {tags.map((obj) => {
+                                return <div key={obj.ID} className="tag">{obj.name}</div>
+                            })}
                         </div>
-                    </div>
+                    }
+
                 </div>
 
                 <div className="dates">
